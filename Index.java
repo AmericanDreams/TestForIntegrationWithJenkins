77b044e1119d4d1bea23343033c3273f1042b9ec
Mirali Fikretzade <miralifikretzade@gmail.com>
2018-11-12 16:57:46 +0400

    Second Commit

--- Index.java
+++ Index.java
@@ -2,7 +2,7 @@
 public class Index {
 	
 	public static void main(String[] args) {
-		System.out.println("Program Worked Baby");
+		System.out.println("Program Worked Baby Bu redakte edildi amma");
 	}
 	
 }
